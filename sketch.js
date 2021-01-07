98c0522262ac8f917c72b66a9f4f9c0bf7a8cd51
Chrislibjorn <christofferholstthomsen@gmail.com>
2021-01-07 08:56:35 +0100

    comit så de fungere

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 function setup() {
     let canvas = createCanvas (400, 400);
-    canvas.parent(game) 
-    slider = createSlider(2, 50, 2);
-    slider.position(windowWidth/2-180,200 );
-    slider.style('width', '80px');
+    canvas.parent(game)
+}
+function draw () {
+    background(1);
+    Fill(255)
 }
